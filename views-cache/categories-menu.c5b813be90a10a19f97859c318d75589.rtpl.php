b7e9df9280e3c19ce46b01966592e2f970316eb7
seteeng <andre.mendes@riogrande.rs.gov.br>
2020-06-05 11:17:04 -0300

    Detalhes do Produto

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/10">Android</a></li><li><a href="/categories/9">Apple</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/10">Android</a></li><li><a href="/categories/9">Apple</a></li><li><a href="/categories/11">Motorola</a></li><li><a href="/categories/12">Samsung</a></li>
